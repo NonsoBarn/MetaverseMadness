773aad0756906953a52076f561aaf258a344a113
NonsoBarn <102404755+NonsoBarn@users.noreply.github.com>
2023-04-28 02:06:29 +0100

    additions

--- project/components/ExploreCard.tsx
+++ project/components/ExploreCard.tsx
@@ -36,14 +36,22 @@ const ExploreCard = ({
           {title}
         </h3>
       ) : (
-        <div className="absolute bottom-0 p-8 justify-start w-full flex-col bg-[rgba(0,0,0.0.5)">
-          <div>
+        <div className="absolute bottom-0 p-8 justify-start w-full flex-col bg-[rgba(0,0,0,0.5)]">
+          <div
+            className={`${styles.flexCenter} w-[60px] h-[60px] rounded-[24px] glassmorphism mb-[16px]`}
+          >
             <img
               src="/headset.svg"
               alt="headset"
               className="w-1/2 h-1/2 object-contain"
             />
           </div>
+          <p className="font-normal text-[16px] leading-[20px] text-white uppercase ">
+            Enter the Metaverse
+          </p>
+          <h2 className="mt-[24px] font-semibold sm:text-[32px] text-[24px] text-white">
+            {title}
+          </h2>
         </div>
       )}
     </motion.div>
